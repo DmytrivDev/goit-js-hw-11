fbd7f58d17dfcbde58fa9e64aec2dc5febf53b1c
DmytrivDev <151439615+DmytrivDev@users.noreply.github.com>
2024-02-11 15:51:27 +0000

    Deploying to gh-pages from @ DmytrivDev/goit-js-hw-11@c2fd86c2131ae0f0612f40c2d0174cff8b1bc183 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,20 @@
-import"./assets/modulepreload-polyfill-ec808ebb.js";import{f,i as h}from"./assets/vendor-651d7991.js";const c=document.getElementById("datetime-picker"),n=document.querySelector(".start-btn");let r;const g={enableTime:!0,time_24hr:!0,defaultDate:new Date,disableMobile:!0,minuteIncrement:1,onClose(e){r=e[0];let t=new Date;r.getTime()<t.getTime()?(n.disabled=!0,h.show({message:"Please choose a date in the future",position:"topRight",backgroundColor:"red",messageColor:"white",theme:"dark",iconUrl:"/goit-js-hw-10/assets/error.svg"})):n.disabled=!1}};f(c,g);function a(e){const l=Math.floor(e/864e5),u=Math.floor(e%864e5/36e5),d=Math.floor(e%864e5%36e5/6e4),m=Math.floor(e%864e5%36e5%6e4/1e3);return{days:l,hours:u,minutes:d,seconds:m}}n.addEventListener("click",b);function b(){n.disabled=!0,c.disabled=!0;const e=new Date().getTime();let t=r.getTime()-e;i(a(t)),t-=1e3;const o=setInterval(()=>{i(a(t)),t-=1e3,t<0&&clearInterval(o)},1e3)}function i(e){Object.keys(e).forEach(function(t){e[t].toString().length<2&&(e[t]="0"+e[t]);const s=document.querySelector("[data-"+t+"]");s.textContent=e[t]})}
+import{S as l,i as c}from"./assets/vendor-46aac873.js";(function(){const n=document.createElement("link").relList;if(n&&n.supports&&n.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const i of t.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&o(i)}).observe(document,{childList:!0,subtree:!0});function s(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function o(e){if(e.ep)return;e.ep=!0;const t=s(e);fetch(e.href,t)}})();const p=new l(".gallery a",{captions:!0,captionSelector:"img",captionType:"attr",captionsData:"alt",captionPosition:"bottom",captionDelay:250}),m=a=>{const n=document.querySelector(".images__cont");let s="";a.hits.map(o=>{s+=`<a href="${o.largeImageURL}" class="imagecont">
+        <div class="imagephoto">
+            <img src="${o.webformatURL}" alt="${o.tags}">
+        </div>
+        <span class="imageinfo">
+            <span>
+                <span>Likes</span><span>${o.likes}</span>
+            </span>
+            <span>
+                <span>Views</span><span>${o.views}</span>
+            </span>
+            <span>
+                <span>Comments</span><span>${o.comments}</span>
+            </span>
+            <span>
+                <span>Downloads</span><span>${o.downloads}</span>
+            </span>
+        </span>
+    </a>`}),n.innerHTML=s,p.refresh()},d=(a,n)=>{const s=document.querySelector(".images__cont");s.innerHTML='<span class="loader"></span>';const o="https://pixabay.com/api/",e={method:"GET"},t=new URLSearchParams({key:"42320940-042fd388efe736bb4087979b1",q:a,image_type:"photo",orientation:"horizontal",safesearch:!0}),i=`${o}?${t}`;fetch(i,e).then(r=>{if(!r.ok)throw new Error(r.status);return r.json()}).then(r=>{r.total===0?(s.innerHTML="",n.show({message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight",backgroundColor:"red",messageColor:"white",theme:"dark",iconUrl:"../img/error.svg"})):m(r)}).catch(r=>{s.innerHTML="",n.show({message:r,position:"topRight",backgroundColor:"red",messageColor:"white",theme:"dark",iconUrl:"../img/error.svg"})})},g=document.querySelector(".form");g.addEventListener("submit",a=>{a.preventDefault();const n=a.target,s=n.elements.field.value;s?(d(s,c),n.reset()):c.show({message:"Please, fill an input",position:"topRight",backgroundColor:"red",messageColor:"white",theme:"dark",iconUrl:"../img/error.svg"})});
 //# sourceMappingURL=commonHelpers.js.map
