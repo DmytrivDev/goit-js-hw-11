4ee63ad714c8066f9469376b6f627c830e142728
DmytrivDev <151439615+DmytrivDev@users.noreply.github.com>
2024-02-11 17:40:00 +0200

    Done

---
+++ src/main.js
@@ -0,0 +1,30 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+import {pixebayApi} from "./js/pixebay-api";
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const target = e.target;
+    const word = target.elements.field.value;
+
+    if(!word) {
+        iziToast.show({
+            message: 'Please, fill an input',
+            position: 'topRight',
+            backgroundColor: 'red',
+            messageColor: 'white',
+            theme: 'dark',
+            iconUrl: '../img/error.svg'
+        });
+    } else { 
+        pixebayApi(word, iziToast);
+
+        target.reset();
+    }
+});
+
+
